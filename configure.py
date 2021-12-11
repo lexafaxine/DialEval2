d8ba4ad0f6fc96dc8d380438be2f03597d160310
lexafaxine <lexafaxine@gmail.com>
2021-12-11 11:47:38 +0900

    modify configure

--- configure.py
+++ configure.py
@@ -23,7 +23,7 @@ def parse_args():
                         help="Dataset shuffle buffer size")
 
     # Task detail
-    parser.add_argument("--mode", default=None, type=str,
+    parser.add_argument("--mode", default="train", type=str,
                         help="Train, evaluate or predict", required=True)
     parser.add_argument("--language", default=None, type=str,
                         help="English or Chinese", required=True)
@@ -31,34 +31,28 @@ def parse_args():
                         help="Nugget or Quality(both lower case)", required=True)
 
     # Preprocessing & Model
-    parser.add_argument("--max_dialogue_length", default=512, type=int,
+    parser.add_argument("--max_len", default=512, type=int,
                         help="Max diague length for dialogue-level model")
-    parser.add_argument("--max_sentence_len", default=100, type=int,
-                        help="Max sentence length for sentence-level model")
     parser.add_argument("--plm", default=None, type=str,
                         help="Pretrained Language Model name: BERT or XLNet", required=True)
 
     # Model Hyper-parameters
-    parser.add_argument("--lstm_hidden_size", default=100, type=int,
-                        help="Dimensionality of RNN hidden (default: 100)")
     parser.add_argument("--transformer_hidden_size", default=768, type=int,
                         help="Dimensionality of Transformer Encode Block(default:768)")
-    parser.add_argument("--ff_size", default=512, type=int,
-                        help="ff size of attention in transformer")
+    parser.add_argument("--ff_size", default=200, type=int,
+                        help="feed forward layer size in transformer")
     parser.add_argument("--heads", default=8, type=int,
                         help="Number of head of the multi head attention")
-    parser.add_argument("--rnn-dropout", default=0.3, type=int,
-                        help="Dropout rate of RNN")
+    parser.add_argument("--layer_num", default=1, type=int,
+                        help="Number transformer encoder")
     parser.add_argument("--dropout", default=0.1, type=int,
                         help="Dropout rate of the inputs of Dense layer")
 
     # Training
-    parser.add_argument("--batch_size", default=32, type=int,
+    parser.add_argument("--batch_size", default=4, type=int,
                         help="Batch Size")
     parser.add_argument("--epoch", default=20, type=int,
                         help="Number of Epoch")
-    parser.add_argument("--embedding", default=None, type=str,
-                        help="Embedding level: sentence or dialogue", required=True)
     parser.add_argument("--warmup", default=1200, type=int,
                         help="warm up steps of optimizer")
 
